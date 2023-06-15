8a5d60c6cd76c18d3be2059fd53c591c245a251e
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-06-16 08:47:10 +0900

    メニューバー実装

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./menu.module.scss";
 import { Tag } from "../../types.ts/blog";
 import { TagItem } from "../tag-item";
+import Image from "next/image";
 
 type Props = {
   tags: Tag[];
 };
 const MenuComponent: React.FC<Props> = ({ tags }) => {
+  const [open, setOpen] = useState(false);
+  const menuClass = open ? "display_block_mobile" : "display_none_mobile";
   return (
-    <div className={styles.menu}>
-      <div>
-        <h2>タグ一覧</h2>
-        {tags.map((tag) => (
-          <TagItem name={tag.tag} key={tag.id} />
-        ))}
+    <>
+      <div className={styles.mobile}>
+        <div className={styles.hamburger} onClick={() => setOpen(!open)}>
+          {open ? (
+            <span className={styles.closedButton}>×</span>
+          ) : (
+            <Image
+              src="/menu_open.png"
+              width={40} // 元の画像の実際の幅を指定
+              height={40} // 元の画像の実際の高さを指定
+              priority
+              style={{ position: "relative" }}
+              alt="thumbnail"
+            />
+          )}
+        </div>
       </div>
-      <div>
-        <h2>プロフィール</h2>
+      <div className={`${menuClass} display_inline_block_pc`}>
+        <div className={styles.menu}>
+          <div className={styles.displayNoneMobile}>
+            <div>
+              <h2>タグ一覧</h2>
+              {tags.map((tag) => (
+                <TagItem name={tag.tag} key={tag.id} />
+              ))}
+            </div>
+            <div>
+              <h2>プロフィール</h2>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
