35f509ab450cce74125fc0db98c3e93da7173184
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-06-25 11:01:32 +0900

    moppyのバナー追加

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./footer.module.scss";
+import { AD_CONTENT_FOOTER } from "../../constants/constants";
+import parse from "html-react-parser";
 
 const FooterComponent = () => {
   return (
     <div className={styles.footer}>
+      {parse(AD_CONTENT_FOOTER)}
+      <br />
       <Link href="/terms/privacy-policy">プライバシーポリシー</Link>
       <p>© 2023 Mike Travel Blog</p>
     </div>
--- constants/constants.ts
+++ constants/constants.ts
@@ -7,4 +7,6 @@ export const COMMON_DESCRIPTION =
 // 広告欄
 export const AD_CONTENT = `<a href="https://px.a8.net/svt/ejp?a8mat=3TAVQ2+DDNP6A+52F8+5ZMCH" rel="nofollow">
 <img border="0" width="300" height="250" alt="" src="https://www25.a8.net/svt/bgt?aid=230615066809&wid=001&eno=01&mid=s00000023642001006000&mc=1"></a>
-<img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3TAVQ2+DDNP6A+52F8+5ZMCH" alt="">`;
+<img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3TAVQ2+DDNP6A+52F8+5ZMCH" alt=""><h3>旅費を安くしたい方へ</h3><a href="https://pc.moppy.jp/entry/invite.php?invite=fXXBe1cd&type=service"><img decoding="async" loading="lazy" src="https://img.moppy.jp/pub/pc/friend/300x250-1.jpg" alt="累計会員数1,000万人突破！内職/副業/お小遣い稼ぎするならモッピー！" width="300" height="250"></a>`;
+
+export const AD_CONTENT_FOOTER = `<a href="https://pc.moppy.jp/entry/invite.php?invite=fXXBe1cd&type=mile"><img decoding="async" loading="lazy" src="https://img.moppy.jp/pub/pc/friend/468x60-8.jpg" alt="稼いだポイントをマイルへ！陸マイラー御用達モッピー" width="468" height="60"></a>`;
