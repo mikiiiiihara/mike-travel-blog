8b667e78cf91fd1fc1952b5aa85afb943f35b1de
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-07-16 02:37:50 +0900

    広告減らす

--- constants/constants.ts
+++ constants/constants.ts
@@ -5,6 +5,4 @@ export const COMMON_DESCRIPTION =
   "お得に海外旅行するための情報、おすすめ旅行先(時々K-POP)をお届け！";
 
 // 広告欄
-export const AD_CONTENT = `<a href="https://px.a8.net/svt/ejp?a8mat=3TAVQ2+DDNP6A+52F8+5ZMCH" rel="nofollow">
-<img border="0" width="300" height="250" alt="" src="https://www25.a8.net/svt/bgt?aid=230615066809&wid=001&eno=01&mid=s00000023642001006000&mc=1"></a>
-<img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3TAVQ2+DDNP6A+52F8+5ZMCH" alt=""><h3>旅費を安くしたい方へ</h3><a href="https://pc.moppy.jp/entry/invite.php?invite=fXXBe1cd&type=service"><img decoding="async" loading="lazy" src="https://img.moppy.jp/pub/pc/friend/300x250-1.jpg" alt="累計会員数1,000万人突破！内職/副業/お小遣い稼ぎするならモッピー！" width="300" height="250"></a>`;
+export const AD_CONTENT = `<img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=3TAVQ2+DDNP6A+52F8+5ZMCH" alt=""><h3>旅費を安くしたい方へ</h3><a href="https://pc.moppy.jp/entry/invite.php?invite=fXXBe1cd&type=service"><img decoding="async" loading="lazy" src="https://img.moppy.jp/pub/pc/friend/300x250-1.jpg" alt="累計会員数1,000万人突破！内職/副業/お小遣い稼ぎするならモッピー！" width="300" height="250"></a>`;
